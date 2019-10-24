4f2683e7b74f195f81a3362b5ba46e6feb626e52
Oskar Stark <oskarstark@googlemail.com>
2019-10-24 08:52:33 +0200

    Check if envs are set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
+[ -z "$SYMFONY_PROJECT_ID" ] && echo "Need to set SYMFONY_PROJECT_ID" && exit 1
+[ -z "$SYMFONY_TOKEN" ] && echo "Need to set SYMFONY_TOKEN" && exit 1
+
 sh -c "/usr/local/bin/symfony link $SYMFONY_PROJECT_ID"
 sh -c "/usr/local/bin/symfony $*"
-
