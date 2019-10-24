a6fb44699df19594d3ec76395337f6e6ecf0ed8c
Oskar Stark <oskarstark@googlemail.com>
2019-10-24 08:59:01 +0200

    printenv to debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 [ -z "$SYMFONY_PROJECT_ID" ] && echo "Need to set SYMFONY_PROJECT_ID" && exit 1
 [ -z "$SYMFONY_TOKEN" ] && echo "Need to set SYMFONY_TOKEN" && exit 1
 
+sh -c "printenv"
+
 sh -c "/usr/local/bin/symfony link $SYMFONY_PROJECT_ID"
 sh -c "/usr/local/bin/symfony $*"
