28fb5612b4d86e55315123c7bd99ac32711bedf4
Oskar Stark <oskarstark@googlemail.com>
2019-10-24 09:51:03 +0200

    use shortcut

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,13 @@
 
 [ -z "$SYMFONY_PROJECT_ID" ] && echo "Need to set SYMFONY_PROJECT_ID" && exit 1
 [ -z "$SYMFONY_TOKEN" ] && echo "Need to set SYMFONY_TOKEN" && exit 1
-[ -z "$GITHUB_REF" ] && echo "Need to set SYMFONY_TOKEN" && exit 1
+[ -z "$GITHUB_REF" ] && echo "Need to set GITHUB_REF" && exit 1
 
 sh -c "printenv"
-
-
 
 SYMFONY_ENVIRONMENT="${GITHUB_REF/refs\/head\//}"
 
 sh -c "symfony version"
 sh -c "symfony link $SYMFONY_PROJECT_ID"
 sh -c "symfony env:create $SYMFONY_ENVIRONMENT"
-sh -c "/usr/local/bin/symfony $*"
+sh -c "symfony $*"
