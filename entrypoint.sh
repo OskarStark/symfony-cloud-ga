55a1a6997676a0a969156b35f7655e7ef6fad7aa
Oskar Stark <oskarstark@googlemail.com>
2019-10-24 09:32:22 +0200

    get feature branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,15 @@
 
 [ -z "$SYMFONY_PROJECT_ID" ] && echo "Need to set SYMFONY_PROJECT_ID" && exit 1
 [ -z "$SYMFONY_TOKEN" ] && echo "Need to set SYMFONY_TOKEN" && exit 1
+[ -z "$GITHUB_REF" ] && echo "Need to set SYMFONY_TOKEN" && exit 1
 
 sh -c "printenv"
 
-sh -c "/usr/local/bin/symfony version"
-sh -c "/usr/local/bin/symfony link $SYMFONY_PROJECT_ID"
+
+
+echo "${GITHUB_REF/refs/head/X}
+
+sh -c "symfony version"
+sh -c "symfony link $SYMFONY_PROJECT_ID"
+sh -c "symfony env:create ${GITHUB_REF/refs\/head\//}"
 sh -c "/usr/local/bin/symfony $*"
